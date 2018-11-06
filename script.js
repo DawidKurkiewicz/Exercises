2b1a4f87fb1587f3d6c2dfc49e87a8417fd63748
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-06 12:17:36 +0100

    task3

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const numbers = [1, 2, 3, 4, 5]
 
-Array.prototype.myOwnMap = function (func) {
-    let newArray  = []
-
-    for (let i = 0; i < this.length; i++){
-        newArray.push(func(this[i], i, this))
+Array.prototype.myOwnFind = function (func) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        if (func(this[i], i, this)) {
+            return this[i]
+        }
     }
-    return newArray
 }
 
-const result = numbers.myOwnMap((e) => 2 * e)
+const result = numbers.find((e) => e % 2 === 0)
+
+console.log(result)
